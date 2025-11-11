ad0ba0e94af949324520b1d057673bbf292588a0
Luke Devlin <lukedevlin45@gmail.com>
2025-11-11 15:45:12 +0000

    Edit the script to include checking

--- natas/natas8_decode.sh
+++ natas/natas8_decode.sh
@@ -4,3 +4,8 @@ ascii=$(echo "$encoded" | xxd -r -p)
 reversed=$(echo "$ascii" | rev)
 secret=$(echo "$reversed" | base64 -d)
 echo "Decoded secret: $secret"
+
+base64encoded=$(echo "$secret" | base64)
+reversebase64=$(echo "$base64encoded" | rev)
+hexoutput=$(echo -n "$reversebase64" | xxd -p)
+echo "Hex output: $hexoutput":w
